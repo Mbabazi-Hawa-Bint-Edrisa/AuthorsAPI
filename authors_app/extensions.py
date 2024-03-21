a9c5721cca8f449978929b2754246087833d50f0
Mbabazi-Hawa-Bint-Edrisa <143553200+Mbabazi-Hawa-Bint-Edrisa@users.noreply.github.com>
2024-03-21 14:16:38 +0600

    Authors API

--- authors_app/extensions.py
+++ authors_app/extensions.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate
 
-# from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 # from flask_jwt_extended import JWTManager
 
@@ -11,5 +11,5 @@ migrate = Migrate()
 
 # db = SQLAlchemy()
 # migrate = Migrate()
-# bcrypt = Bcrypt()
+bcrypt = Bcrypt()
 # jwt = JWTManager()
--- authors_app/models/user.py
+++ authors_app/models/user.py
@@ -10,8 +10,22 @@ class User(db.Model):
     last_name=db.Column(db.String(100),nullable=False)
     email=db.Column(db.String(100),nullable=False,unique=True)
     contact=db.Column(db.Integer,unique=True)
-    user_type=db.Column(db.String(100),nullable=False)
+    password=db.Column(db.String(100),nullable=False,unique=True)
+    #biography=db.Column(db.String(100),nullable=False,unique=True)
+    #user_type=db.Column(db.String(100),nullable=False)
     image=db.Column(db.String(255),nullable=False)
     created_at=db.Column(db.DateTime,default=datetime.now())
     updated_at=db.Column(db.DateTime,onupdate=datetime.now())
     #books=db.relationship('Book',backref='user')
+    
+    def __init__(self,first_name,last_name,user_type,password,email,contact,image):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email= email
+        self.contact= contact
+        self.image= image
+        self.password=password
+        self.user_type=user_type
+
+    def get_full_name(self):
+        return f'{self.last_name} {self.first_name}'
